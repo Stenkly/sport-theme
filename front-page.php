1503e5b8fe95b9b3bca34d842dca5dbe09e4923f
Stenkly <stenkly@users.noreply.github.com>
2026-05-29 23:52:57 +0200

    Aumentata altezza footer (padding 20px) e font size a 13px

--- front-page.php
+++ front-page.php
@@ -183,11 +183,11 @@
     .hp-footer-copy {
         background: #F9EA86;
         color: #000;
-        font-size: 12px;
+        font-size: 13px;
         font-weight: 700;
         text-transform: uppercase;
         letter-spacing: 1px;
-        padding: 16px 20px;
+        padding: 20px 20px;
         display: flex;
         justify-content: center;
         align-items: center;
