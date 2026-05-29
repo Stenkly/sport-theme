89a0418cfa5f4f835ac3c2730618c000ac187660
Stenkly <stenkly@users.noreply.github.com>
2026-05-29 22:26:36 +0200

    Allineamento data e ora su singola riga e riduzione box grigio prossimi incontri

--- template-prima-squadra.php
+++ template-prima-squadra.php
@@ -236,7 +236,7 @@ get_header();
             ?>
             <div class="match-card">
                 <div class="match-info">
-                    <p class="match-date text-white"><span class="match-date-day"><?php echo esc_html($formatted_date); ?></span><span class="match-date-time"><?php echo esc_html($ora_p); ?></span></p>
+                    <p class="match-date text-white"><span class="match-date-day"><?php echo esc_html($formatted_date); ?>, </span><span class="match-date-time">ore <?php echo esc_html($ora_p); ?></span></p>
                     <p class="match-venue"><?php echo esc_html($stadio); ?></p>
                 </div>
                 <div class="match-teams">
